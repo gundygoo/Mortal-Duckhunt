7d7210df50c4fdfb6d7c0465b61894990f41a46f
Erik Gunderson <gundy.goo@gmail.com>
2016-01-29 14:20:21 -0600

    spawn birds script

--- Assets/Scripts/SpawnBirds.cs
+++ Assets/Scripts/SpawnBirds.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class SpawnBirds : MonoBehaviour {
 
+    public int timer = 15;
+    public GameObject bird;
+    public GameObject spawnPoint;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
+    void Spawn()
+    {
+        Instantiate(bird, transform.position, Quaternion.identity);
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        timer -= 1;
+        if (timer == 0)
+        {
+            Spawn();
+            timer = 15;
+        }
 	}
 }
