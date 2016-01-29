f57d0a9653ec934e5e0688c3fbb61817d13bd68d
podbornym <podbornym0542@my.uwstout.edu>
2016-01-29 13:45:21 -0600

    Initial scene creation || Script creation

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScipt : MonoBehaviour {
+    public SceneController sceneController;
+    public int playerHealth;
+    public bool powerUp;
+    
+	
+    // Use this for initialization
+	void Start () {
+        playerHealth = 100;
+        powerUp = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Get mouse
+        
+
+        // Throw spear
+        if(Input.GetMouseButton(0))
+        {
+            // throw spear at mouse position
+            // get mouse position
+            // throw spear at position
+        }
+
+        // Swing sword
+        if (Input.GetMouseButton(1))
+        {
+            // swing
+        }
+
+        // Use Zeus' lightning (powerup)
+        if (Input.GetKeyDown("space")) //KeyCode.Space
+        {
+            powerUp = false;
+        }
+	}
+}
