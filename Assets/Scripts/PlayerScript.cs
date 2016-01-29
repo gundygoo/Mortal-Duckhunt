8d8ee1f90e84cc7815bdae2b5771f34fa287f9ec
podbornym <podbornym0542@my.uwstout.edu>
2016-01-29 14:43:38 -0600

    Some minor player/spear changes

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScipt : MonoBehaviour {
+public class PlayerScript : MonoBehaviour {
     public SceneController sceneController;
     public int playerHealth;
     public bool powerUp;
-    
+    public GameObject spearHand;
+    public GameObject spear;
+    //public GameObject player;
+    //public GameObject swordHand;
+    //static Vector3 mousePos = Input.mousePosition;
 	
     // Use this for initialization
 	void Start () {
@@ -16,27 +20,39 @@ public class PlayerScipt : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    // Get mouse
-        
+        float mouseX = (Input.mousePosition.x);
+        float mouseY = (Input.mousePosition.y);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 0));
+        print(mousePos);
 
         // Throw spear
         if(Input.GetMouseButton(0))
         {
+            print("true");
             // throw spear at mouse position
             // get mouse position
             // throw spear at position
+            //print position of mouse
+           
+            //Vector3 mousePos = Input.mousePosition;
+            //targetPos = 
+            Instantiate(spear, transform.position, Quaternion.identity);
+            //spear.transform.Translate(new Vector3(0.0f, 0.0f, 0.0f));
         }
-
+        /*
         // Swing sword
         if (Input.GetMouseButton(1))
         {
-            // swing
+            // swing sword
         }
 
         // Use Zeus' lightning (powerup)
         if (Input.GetKeyDown("space")) //KeyCode.Space
         {
-            powerUp = false;
+            // Create lighting
+            // Defeat enemies on-screen
+            //powerUp = false;
         }
+         * */
 	}
 }
---
+++ Assets/Scripts/SpearScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpearScript : MonoBehaviour {
+    public SceneController sceneController;
+    //public int playerHealth;
+    //public bool powerUp;
+    public GameObject spear;
+    public Transform target;
+    public float speed;
+    //public GameObject player;
+    //public GameObject swordHand;
+    //static Vector3 mousePos = Input.mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = (Input.mousePosition.x);
+        float mouseY = (Input.mousePosition.y);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 0));
+        print(mousePos);
+
+        spear.transform.Translate(new Vector3(0.0f, 0.0f, 0.0f));
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        print(transform.position);
+    }
+}
