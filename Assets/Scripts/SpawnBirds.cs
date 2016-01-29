42e7a74296a2013abca73ab38a07449803e3db33
Erik Gunderson <gundy.goo@gmail.com>
2016-01-29 15:04:31 -0600

    bird move and spawn stuff

--- Assets/Scripts/SpawnBirds.cs
+++ Assets/Scripts/SpawnBirds.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class SpawnBirds : MonoBehaviour {
 
-    public int timer = 15;
+    public float timer =  5;
     public GameObject bird;
-    public GameObject spawnPoint;
+    public int birdCount = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +15,23 @@ public class SpawnBirds : MonoBehaviour {
     void Spawn()
     {
         Instantiate(bird, transform.position, Quaternion.identity);
+        birdCount += 1;
     }
 
 	// Update is called once per frame
 	void Update () {
-        timer -= 1;
-        if (timer == 0)
+        if (birdCount < 10)
         {
-            Spawn();
-            timer = 15;
+            timer -= Time.deltaTime;
+        }
+        
+        if (timer <= 0)
+        {
+            if (birdCount < 10)
+            {
+                Spawn();
+                timer = 10;
+            }
         }
 	}
 }
