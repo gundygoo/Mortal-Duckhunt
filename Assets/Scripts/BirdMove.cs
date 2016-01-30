09f31397e88ce5721194c0651ce0490df5c64c7d
joshxdaniels <joshxdaniels@gmail.com>
2016-01-30 14:28:49 -0600

    Scene 3 has sword bounding box

--- Assets/Scripts/BirdMove.cs
+++ Assets/Scripts/BirdMove.cs
@@ -17,12 +17,22 @@ public class BirdMove : MonoBehaviour {
 
 	}
 
-	/*void OnCollisionEnter2D(Collision2D other)
+	void OnCollisionEnter2D(Collision2D other)
 	{
 		if(other.gameObject.tag=="spearobjectname")
 		{
-			fuse both objects and set gravity
-			negate the interaction with other objects (prevent it from damaging the player, reduce the number of remaining birds)
+			//fuse both objects and set gravity
+
+			
+
+			//negate the interaction with other objects (prevent it from damaging the player)
+			// Right now it just destroys the spear, we'll do more later
+			Destroy (gameObject);
+
+			// Reduce spawners bird count
+			GameObject Spawner = GameObject.Find("Spawn1");
+			SpawnBirds spawnBirds = Spawner.GetComponent<SpawnBirds>();
+			spawnBirds.birdCount-=1;
 		}
-	}*/
+	}
 }
