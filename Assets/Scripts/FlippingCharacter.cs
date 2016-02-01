20917fe308942a163a34464cd4f922080fd7fd22
KollmansbergerK <kollmansbergerk0160@my.uwstout.edu>
2016-02-01 16:58:03 -0600

    Flipping
    I made the game object player_1 flip if the mouse position passes the
    player_1 x position

---
+++ Assets/Scripts/FlippingCharacter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlippingCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 flip = transform.localScale;
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        float WorldxPos = Camera.main.ScreenToWorldPoint(mousePosition).x;
+        if (WorldxPos > gameObject.transform.position.x)
+        {
+            flip.x = -1;
+            transform.localScale = flip;
+        }
+        else
+        {
+            flip.x = 1;
+            transform.localScale = flip;
+        }
+
+    }
+    
+}
