86c3695a4be5745938116b4002c041c7266a15c3
podbornym <podbornym0542@my.uwstout.edu>
2016-02-12 13:47:30 -0600

    Score Scene

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    public void LoadNextLevel()
+    {
+        int i = Application.loadedLevel;
+        Application.LoadLevel(i + 1);
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -14,25 +14,28 @@ public class Timer : MonoBehaviour {
         {
             timer = 60;
         }*/
-        timer = 60;
+        timer = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         timer -= Time.deltaTime;
-        timerSlider.value = timer;
+        
+        if(Application.loadedLevelName != "ScoreScene")
+            timerSlider.value = timer;
 
         if(timer <= 0)
         {
             isPlayable = false;
             print("End game");
-            if (Application.loadedLevelName == "Level1")
+            /*if (Application.loadedLevelName == "Level1")
                 Application.LoadLevel("Level1Poseidon");
             if (Application.loadedLevelName == "Level1Poseidon")
-                Application.LoadLevel("Hades Level");
-            curtain.transform.position = Vector3.MoveTowards(curtain.transform.position, new Vector3(0, 0, -5), .5f);
-            gameObject.GetComponent<SceneController>().TallyScore();
-            
+                Application.LoadLevel("Hades Level");*/
+            //curtain.transform.position = Vector3.MoveTowards(curtain.transform.position, new Vector3(0, 0, -5), .5f);
+            //before curtain is done moving, add score to scoreKeeper.
+            //gameObject.GetComponent<ScoreKeeper>().ScoreTally();
+            Application.LoadLevel(Application.loadedLevel + 1);
         }
 	}
 }
