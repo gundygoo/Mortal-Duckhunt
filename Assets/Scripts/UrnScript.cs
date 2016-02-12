5559616480260f5f206c3d831d760439e08ceb49
joshxdaniels <joshxdaniels@gmail.com>
2016-02-12 14:24:00 -0600

    Added the powerup getter thing,
    yo.

---
+++ Assets/Scripts/UrnScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UrnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Spear") {
+			Destroy(other.gameObject);
+			Destroy (gameObject);
+			GameObject.Find ("player_character").GetComponent<PlayerScript> ().powerUp = true;
+			print ("PowerUp status is: " + GameObject.Find ("player_character").GetComponent<PlayerScript> ().powerUp);
+		}
+	}
+}
