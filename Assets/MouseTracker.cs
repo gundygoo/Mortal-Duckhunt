2d8d5f440626b9bcd57b2e73bdd33768f17c6eda
podbornym <podbornym0542@my.uwstout.edu>
2016-02-14 16:30:19 -0600

    Added cursor prefab

--- Assets/MouseTracker.cs
+++ Assets/MouseTracker.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class MouseTracker : MonoBehaviour {
+    public GameObject cursor;
+    public Vector3 cursorPos;
 
 	// Use this for initialization
 	void Start () {
-	
+        //Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorPos.z = -9;
+        cursor.transform.position = cursorPos;
 	}
 }
