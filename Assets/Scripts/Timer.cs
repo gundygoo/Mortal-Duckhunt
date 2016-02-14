288412a57d02910e4d04d868670f947e138083e1
podbornym <podbornym0542@my.uwstout.edu>
2016-02-14 16:00:53 -0600

    Timer w/curtain fix

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,10 +7,12 @@ public class Timer : MonoBehaviour {
     public float timer;
     public bool isPlayable = true;
     public GameObject curtain;
+    GameObject[] birdList;
 
 	// Use this for initialization
 	void Start () {
-        timer = 60;
+        timer = 5;
+        curtain = GameObject.Find("curtain");
 	}
 	
 	// Update is called once per frame
@@ -23,15 +25,25 @@ public class Timer : MonoBehaviour {
         if(timer <= 0)
         {
             isPlayable = false;
-            print("End game");
+            birdList = GameObject.FindGameObjectsWithTag("Bird");
+            if (birdList.Length == 0)
+            {
+                curtain.transform.position = Vector3.MoveTowards(curtain.transform.position, new Vector3(0, 0, -5), .5f);
+            }
+
+            if (curtain.transform.position.Equals(new Vector3(0, 0, -5)))
+            {
+                Application.LoadLevel(Application.loadedLevel + 1);
+            }
+
             /*if (Application.loadedLevelName == "Level1")
                 Application.LoadLevel("Level1Poseidon");
             if (Application.loadedLevelName == "Level1Poseidon")
                 Application.LoadLevel("Hades Level");*/
+
             //curtain.transform.position = Vector3.MoveTowards(curtain.transform.position, new Vector3(0, 0, -5), .5f);
             //before curtain is done moving, add score to scoreKeeper.
             //gameObject.GetComponent<ScoreKeeper>().ScoreTally();
-            Application.LoadLevel(Application.loadedLevel + 1);
         }
 	}
 }
