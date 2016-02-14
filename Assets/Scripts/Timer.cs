935ccf8961c753bbc1af7b9730c6c90856ab46d9
Erik Gunderson <gundy.goo@gmail.com>
2016-02-14 16:23:04 -0600

    poseidon and build

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -11,7 +11,7 @@ public class Timer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        timer = 5;
+        timer = 60;
         curtain = GameObject.Find("curtain");
 	}
 	
